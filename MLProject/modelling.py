d1fcd8ec01af1139f5f3b3883b13df49d6464756
Devina Rahma Aprilia <devinaaprilia1345@gmail.com>
2025-12-17 21:11:11 +0700

    Finalize MLflow project

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-# Load dataset
 df = pd.read_csv("phoneprice_preprocessing.csv")
 
 X = df.drop(columns=["price_range"])
@@ -15,30 +15,29 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
-model = LogisticRegression(
-    C=1.0,
-    solver="lbfgs",
-    max_iter=1000
-)
-model.fit(X_train, y_train)
+with mlflow.start_run():
 
-# Prediction
-y_pred = model.predict(X_test)
+    model = LogisticRegression(
+        C=1.0,
+        solver="lbfgs",
+        max_iter=1000
+    )
+    model.fit(X_train, y_train)
 
-# Metrics
-acc = accuracy_score(y_test, y_pred)
-prec = precision_score(y_test, y_pred, average="weighted")
-rec = recall_score(y_test, y_pred, average="weighted")
-f1 = f1_score(y_test, y_pred, average="weighted")
+    y_pred = model.predict(X_test)
 
-# Manual logging 
-mlflow.log_param("C", 1.0)
-mlflow.log_param("solver", "lbfgs")
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average="weighted")
+    rec = recall_score(y_test, y_pred, average="weighted")
+    f1 = f1_score(y_test, y_pred, average="weighted")
 
-mlflow.log_metric("accuracy", acc)
-mlflow.log_metric("precision", prec)
-mlflow.log_metric("recall", rec)
-mlflow.log_metric("f1_score", f1)
+    mlflow.log_param("C", 1.0)
+    mlflow.log_param("solver", "lbfgs")
 
-mlflow.sklearn.log_model(model, "model")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("precision", prec)
+    mlflow.log_metric("recall", rec)
+    mlflow.log_metric("f1_score", f1)
+
+    mlflow.sklearn.log_model(model, "model")
+
