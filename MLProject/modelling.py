5efc76c70a1aa2cddbe6671a44129efbb1092af3
Devina Rahma Aprilia <devinaaprilia1345@gmail.com>
2025-12-17 19:00:42 +0700

    Add MLflow CI workflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+df = pd.read_csv("phoneprice_preprocessing.csv")
+
+#split train test
+X = df.drop(columns=["price_range"])
+y = df["price_range"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+mlflow.set_experiment("Phone Price Classification - Basic")
+
+with mlflow.start_run():
+
+    # training tanpa tuning
+    model = LogisticRegression(
+        C=1.0,
+        solver="lbfgs",
+        max_iter=1000
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average="weighted")
+    rec = recall_score(y_test, y_pred, average="weighted")
+    f1 = f1_score(y_test, y_pred, average="weighted")
+
+    # manual logging
+    mlflow.log_param("C", 1.0)
+    mlflow.log_param("solver", "lbfgs")
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("precision", prec)
+    mlflow.log_metric("recall", rec)
+    mlflow.log_metric("f1_score", f1)
+
+    mlflow.sklearn.log_model(model, "model")
